131a87c1c5cbd4849d9866e7cc10fa73b308d962
Liubov Karpenko <simonagape13@gmail.com>
2023-11-07 13:56:14 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,9 @@ from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 user_model = get_user_model()
 from blog.models import Post
+import logging
+
+logger = logging.getLogger(__name__)
 
 register = template.Library()
 
@@ -48,6 +51,7 @@ def endcol():
     return format_html("</div>")
 
 @register.inclusion_tag("blog/post-list.html")
-def recent_posts(post):
+def recent_posts(post):  
   posts = Post.objects.exclude(pk=post.pk)[:5]
+  logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
   return {"title": "Recent Posts", "posts": posts}
