ba557048e34a736a493f31a25ac7d9b3a14fb959
Liubov Karpenko <simonagape13@gmail.com>
2023-12-10 13:57:11 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -78,6 +78,7 @@ class Dev(Configuration):
         "rest_framework",
         "rest_framework.authtoken",
         "drf_yasg",
+        "django_filters",
     ]
 
     MIDDLEWARE = [
@@ -229,7 +230,7 @@ class Dev(Configuration):
         ],
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-        ]
+        ],
         "DEFAULT_THROTTLE_CLASSES": [
             "blog.api.throttling.AnonSustainedThrottle",
             "blog.api.throttling.AnonBurstThrottle",
@@ -242,7 +243,12 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
-
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 
     SWAGGER_SETTINGS = {
